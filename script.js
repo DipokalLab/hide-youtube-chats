dc0748ca0fb60465eb2262710e483b969110543b
Dipokal HHJ <hhj@devent.kr>
2023-08-21 14:11:43 +0900

    feat: move box

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 
 class HideBox {
     constructor() {
+        this.isMove = false
+        this.position = { x: 0, y: 0 }
+        this.clickPosition = { x: 0, y: 0 }
         this.createBox()
+        this.box.addEventListener("mousedown", this.enableMove.bind(this))
+        document.addEventListener("mouseup", this.disableMove.bind(this))
+        document.addEventListener("mousemove", this.move.bind(this))
+
     }
 
     createBox() {
@@ -15,6 +22,30 @@ class HideBox {
         this.box.style.zIndex = '10000000000'
     }
 
+    updatePosition({ x, y }) {
+        this.box.style.top = x - this.clickPosition.y + 'px'
+        this.box.style.left = y - this.clickPosition.x + 'px'
+    }
+
+    move(e) {
+        if (this.isMove == false) {
+            return 0
+        }
+
+        console.log(e)
+
+        this.updatePosition({ x:  e.clientY, y: e.clientX })
+    }
+
+    enableMove(e) {
+        this.clickPosition.x = e.layerX
+        this.clickPosition.y = e.layerY
+        this.isMove = true
+    }
+
+    disableMove() {
+        this.isMove = false
+    }
 
     render() {
         return this.box
